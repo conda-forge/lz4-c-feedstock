2454d4129cd53b595b1f72a5f7331fa65ecc2f1b
Ray Donnelly <mingw.android@gmail.com>
2020-04-16 14:02:44 +0200

    Windows work (in progress)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,7 +14,8 @@ msbuild.exe ^
   -m ^
   -p:Configuration=%CONFIGURATION% ^
   -p:Platform=%PLATFORM% ^
-  -p:PlatformToolset=v142 ^
+  -p:PlatformToolset=v141 ^
+  -p:WindowsTargetPlatformVersion=10.0.17763.0 ^
   -t:Build ^
   %VSPROJ_DIR%\lz4.sln
 if errorlevel 1 exit 1
